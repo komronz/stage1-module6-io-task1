20bafb39dccd1c0aaab2e57ff40453d1181c0e4f
komronz <pandacoder0820@gmail.com>
2022-09-26 18:45:31 +0500

    done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,30 +2,27 @@ package com.epam.mjc.io;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
-
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
         HashMap<String, String> map = new HashMap<>();
         String line;
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file));
-            while ((line = bufferedReader.readLine()) != null){
-                String[] keyValuePair = line.split(":",2);
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            while ((line = reader.readLine()) != null) {
+                String[] keyValuePair = line.split(": ", 2);
                 String key = keyValuePair[0];
                 String value = keyValuePair[1];
-                map.put(key,value);
+                map.put(key, value);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
         return new Profile(map.get("Name"),
                 Integer.valueOf(map.get("Age")),
-                map.get("Email"),
-                Long.valueOf(map.get("Phone")));
+                map.get("Email"), Long.valueOf(map.get("Phone")));
     }
 }
+
